643763f14a7768cc16e9a04ee623d7fb9bccaf7a
stanleyChinonso <stancharlesdon@gmail.com>
2022-06-10 15:03:22 +0100

    created form

---
+++ src/Components/Form.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div className='form-box'>
+        <form>
+            <input type='email' name='email' placeholder='Email Adress' required></input>
+            hhhhhh
+        </form>
+    </div>
+  )
+}
+
+export default Form
